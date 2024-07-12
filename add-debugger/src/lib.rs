23f8e89de507485b4fdeb5f24706210e25e759ae
joii <joii@cryptape.com>
2024-07-12 22:37:21 +0800

    Support add debugger

---
+++ add-debugger/src/lib.rs
@@ -0,0 +1,12 @@
+ckb_std::entry_simulator!(program_entry);
+ckb_std::default_alloc!();
+
+/// program entry
+fn program_entry() -> i8 {
+    // Call main function and return error code
+    // match {{contract_crate_name}}::entry::main() {
+    //     Ok(_) => 0,
+    //     Err(err) => err as i8,
+    // }
+    0
+}
