f94d71778739af4445b47eef28c66a56c9ff274f
joii <joii@cryptape.com>
2024-07-16 17:21:05 +0800

    Auto add feature simulator to contract

--- add-debugger/src/contract-lib.rs
+++ add-debugger/src/contract-lib.rs
@@ -1,10 +1,6 @@
 #![cfg_attr(not(feature = "simulator"), no_std)]
 
 // This file will be automatically placed in the contract file, and rename to lib.rs
-// TODO
-//  Need to export main function.
-//  At the same time, you need to add a feature to the contract's Cargo.toml.
-//      simulator = ["ckb-std/simulator"]
 
 // #[cfg(feature = "simulator")]
 // pub mod entry;
