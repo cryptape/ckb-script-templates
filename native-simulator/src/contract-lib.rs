39da9513465a657d31e83b6aa5695c238e5dc1f9
joii <joii@cryptape.com>
2024-09-06 17:48:02 +0800

    Contract add feature: native-simulator  * Contracts support native-simulator by default.  * native-simulator project no longer requires scripts to add code to contracts.  * simulator renamed to native-simulator.

--- native-simulator/src/contract-lib.rs
+++
@@ -1,7 +0,0 @@
-#![cfg_attr(not(feature = "simulator"), no_std)]
-#![allow(special_module_name)]
-#![allow(unused_attributes)]
-#[cfg(feature = "simulator")]
-mod main;
-#[cfg(feature = "simulator")]
-pub use main::program_entry;
