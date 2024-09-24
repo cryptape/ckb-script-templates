a0a0d4e4d9ee89a0e5f91b6d1065a604476dff68
joii2020 <joii@cryptape.com>
2024-09-24 15:05:19 +0800

    Optimize cargo-generate rules in native-simulator lib.rs

--- native-simulator/src/lib.rs
+++ native-simulator/src/lib.rs
@@ -1,1 +1,1 @@
-ckb_std::entry_simulator!({{project-name | remove: "-sim" | replace: "-", "_"}}::program_entry);
+ckb_std::entry_simulator!({{project-name | append: "@@SIMULATOR_PLACEHOLDER@@" | remove: "-sim@@SIMULATOR_PLACEHOLDER@@" | replace: "-", "_"}}::program_entry);
