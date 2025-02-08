bd74c94cf36d6f85a5612836ae08c88f6d8d3773
Xuejie Xiao <xxuejie@gmail.com>
2025-02-08 14:51:02 +0800

    Bump heap size for default alloc (#28)
    * Bump heap size for default alloc
    
    The default parameters to setup heap in ckb-std has been too
    convervative, this change bumps the heap size to a reasonable
    configuration, so we are less likely to encounter problems
    
    * Fix typo

--- standalone-contract/src/main.rs
+++ standalone-contract/src/main.rs
@@ -8,11 +8,15 @@ extern crate alloc;
 mod tests;
 
 #[cfg(not(test))]
-use ckb_std::default_alloc;
-#[cfg(not(test))]
 ckb_std::entry!(program_entry);
 #[cfg(not(test))]
-default_alloc!();
+// By default, the following heap configuration is used:
+// * 16KB fixed heap
+// * 1.2MB(rounded up to be 16-byte aligned) dynamic heap
+// * Minimal memory block in dynamic heap is 64 bytes
+// For more details, please refer to ckb-std's default_alloc macro
+// and the buddy-alloc alloc implementation.
+ckb_std::default_alloc!(16384, 1258306, 64);
 
 pub fn program_entry() -> i8 {
     ckb_std::debug!("This is a sample contract!");
